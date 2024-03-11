7432dc6b0efe663109888298c8ab4c00219d9e6f
Николай <kolya-91@list.ru>
2024-03-11 14:12:55 +0500

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println(" Задача 1");
+        String firstName = "Семён ";
+        String middleName = "Иванов ";
+        String lastName = "Семёнович";
+        String fullName = middleName + firstName + lastName;
+        System.out.println(" Ф.И.О. сотрудника " + fullName);
+        // Задача 2
+        System.out.println(" Задача 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        // Задача 3
+        System.out.println(" Задача 3");
+        String fullName2 = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
 
     }
 }
